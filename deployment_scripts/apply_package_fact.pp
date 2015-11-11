bf9a440589856f6db6a4c1c59104a37b78e318a7
Alex Schultz <aschultz@mirantis.com>
2015-11-11 15:16:24 -0600

    Initial commit

---
+++ deployment_scripts/apply_package_fact.pp
@@ -0,0 +1,10 @@
+notice('MOULAR: apply_package_fact.pp')
+
+file { ['/etc/facter', '/etc/facter/facts.d']:
+  ensure => 'directory'
+}
+
+file { '/etc/facter/facts.d/os_package_type.txt':
+  ensure  => 'file',
+  content => 'os_package_type=ubuntu'
+}
