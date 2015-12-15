f2e6bf695aa343a80ca9a6add5d93373e9941595
Alex Schultz <aschultz@mirantis.com>
2015-12-15 16:07:33 -0700

    os_package_type now supported by fuel

--- deployment_scripts/apply_package_fact.pp
+++ deployment_scripts/apply_package_fact.pp
@@ -1,10 +1,21 @@
 notice('MOULAR: fuel-plugin-upstream/apply_package_fact.pp')
 
-file { ['/etc/facter', '/etc/facter/facts.d']:
-  ensure => 'directory'
+$hiera_dir = '/etc/hiera/override'
+$plugin_name = 'upstream'
+$plugin_yaml = "${plugin_name}.yaml"
+
+$override_content = "---\nos_package_type: ubuntu\n"
+
+file { $hiera_dir:
+  ensure => directory,
+}
+file { "${hiera_dir}/${plugin_yaml}":
+  ensure  => file,
+  content => $override_content
 }
 
-file { '/etc/facter/facts.d/os_package_type.txt':
-  ensure  => 'file',
-  content => 'os_package_type=ubuntu'
+file_line { "${plugin_name}_hiera_ovverride":
+  path  => '/etc/hiera.yaml',
+  line  => "  - overide/${plugin_name}",
+  after => '  - ovveride/plugin'
 }
